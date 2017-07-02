d0d40c944ed04e92b6da68d48a34ca59252a764d
Jase-Omeileo West <omeileo@icloud.com>
2017-07-01 23:39:54 -0500

    TableView and Arrays, Multiple View Controllers
    Displaying elements of an array in a tableview and going to a next view
    to show a more detailed version of the selected element.

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    @IBOutlet weak var emojiTableView: UITableView!
+    var emojis = ["🤔", "😀", "☺️", "😘", "🤑", "😫"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        emojiTableView.dataSource = self
+        emojiTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return emojis.count
     }
 
-    override func didReceiveMemoryWarning() {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
